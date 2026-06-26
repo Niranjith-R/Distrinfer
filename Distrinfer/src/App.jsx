2c6898d252dd526548e4252780f848d667393a01
Niranjith R Pilli <nrpniranjith1@gmail.com>
2026-06-26 13:27:51 +0530

    Started working on Front-end :

---
+++ Distrinfer/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+
+
+const App = () => {
+  return (
+	<div className='min-h-screen bg-[#2f3241] text-white'>
+
+		<Sidebar />
+
+	</div>
+  )
+}
+
+export default App
---
+++ Distrinfer/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='fixed left-0 top-0 h-screen w-75 bg-[#272935]'>Sidebar</div>
+  )
+}
+
+export default Sidebar
